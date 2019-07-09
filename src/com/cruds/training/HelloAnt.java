0b366ba93ebf82c784f31d528101ed4bc9aafb01
rishithSadashiv <rishith.1si16ec078@gmail.com>
2019-07-09 21:49:51 +0530

    initial

---
+++ src/com/cruds/training/HelloAnt.java
@@ -0,0 +1,10 @@
+package com.cruds.training;
+
+public class HelloAnt
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Ant****");
+		System.out.println("another &&&&&&");
+	}
+}
